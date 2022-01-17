1b4f4e0879c7ed4eeab10866ecacfc8c11a1ec60
vasoya-benzatine <vasoya@benzatine.com>
2022-01-17 11:11:30 +0530

    first

---
+++ users/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+from .forms import LoginForm
+
+
+def login_page(request):
+    forms = LoginForm()
+    if request.method == 'POST':
+        forms = LoginForm(request.POST)
+        if forms.is_valid():
+            username = forms.cleaned_data['username']
+            password = forms.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('dashboard')
+            messages.error(request,'Please Enter Valid Credentials')   
+            # return render(request, 'users/login.html') 
+    context = {'form': forms}
+    return render(request, 'users/login.html', context)
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('login')
